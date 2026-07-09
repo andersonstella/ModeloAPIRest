3600f292d6d0ab0cd8d71199c43732452def9f71
astella <astella@fh.com.br>
2026-07-09 16:28:55 -0300

    feat: Tarefas

---
+++ src/main/java/taskmanager/domain/Task.java
@@ -0,0 +1,21 @@
+package taskmanager.domain;
+
+import org.springframework.util.Assert;
+
+import java.util.Optional;
+
+public class Task {
+
+    private TaskId id;
+    private String title;
+    private Optional<String> description;
+    private TaskStatus status;
+
+    public Task(String title, Optional<String> description, TaskStatus status) {
+        Assert.notNull(title, "Title must not be null");
+        this.id = new TaskId();
+        this.title = title;
+        this.description = description;
+        this.status = TaskStatus.PENDING;
+    }
+}
